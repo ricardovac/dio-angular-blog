fd3fba700ab8930b04c0a4d7f4019809dc78fbe2
ricardovac <ricardoviicius@gmail.com>
2023-10-13 08:30:48 -0300

    chore: dynamic components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.scss']
+  styleUrls: ['./big-card.component.scss'],
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
+  @Input()
+  photoCover: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string =
+    'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia.';
 
+  constructor() {}
+
+  ngOnInit() {}
 }
