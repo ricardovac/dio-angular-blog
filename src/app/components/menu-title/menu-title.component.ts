688f5b6b94d13d6cf1743775766ad46359ceba65
ricardovac <ricardoviicius@gmail.com>
2023-10-13 07:09:42 -0300

    feat: creating menu-title

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-title',
+  templateUrl: './menu-title.component.html',
+  styleUrls: ['./menu-title.component.scss'],
+})
+export class MenuTitleComponent {}
